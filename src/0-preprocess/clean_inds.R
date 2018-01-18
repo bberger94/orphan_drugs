24b1260a71bd4310727e9ea944ee81ec1689246f
Ben Berger <benjaminaberger94@gmail.com>
2018-01-18 18:05:51 -0500

    Renamed variables with '_'s; forced use of dplyr::filter

--- src/0-preprocess/clean_inds.R
+++ src/0-preprocess/clean_inds.R
@@ -1,19 +1,28 @@
 
 # Clean orphan drug indication data
 
-
 ## Import orphan indications
 orphan_inds <-
   read_excel('data/orphan_indications/orphan_indications_extract_01-18-18.xlsx') 
 
-## Initial cleaning step:
-## 1. Code NAs
+
+## Initial cleaning steps:
+### 1. Rename variables with underscores rather than spaces
+names(orphan_inds) <- 
+  names(orphan_inds) %>% 
+  tolower() %>% 
+  gsub(x = ., pattern = ' ', replacement = '_')
+
+### 2. Code NAs 
+orphan_inds <- 
+  orphan_inds %>% 
+  mutate(trade_name = replace(trade_name, trade_name == 'n/a', NA)) 
+  
 ## 2. Lower case all drug names
 orphan_inds <- 
   orphan_inds %>% 
-  mutate(`Trade Name` = replace(`Trade Name`, `Trade Name` == 'n/a', NA)) %>% 
-  mutate_at(.vars = vars(`Generic Name`, `Trade Name`), funs(tolower)) %>%
-  arrange(`Trade Name`) 
+  mutate_at(.vars = vars(generic_name, trade_name), funs(tolower)) %>%
+  arrange(trade_name) 
 
 
 ## Make manual changes
@@ -21,25 +30,25 @@ orphan_inds <-
   orphan_inds %>%
   mutate(
     
-    `Generic Name` = replace(`Generic Name`, `Generic Name` == "dabrafenib and trametinib", "trametinib and dabrafenib")
+    generic_name = replace(generic_name, generic_name == 'dabrafenib and trametinib', 'trametinib and dabrafenib')
     
-    ,`Trade Name`   = replace(`Trade Name`, `Generic Name` == "trametinib and dabrafenib", "mekinist and tafinlar")
+    ,trade_name   = replace(trade_name, generic_name == 'trametinib and dabrafenib', 'mekinist and tafinlar')
     
-    ,`Generic Name` = replace(`Generic Name`, `Trade Name` == "actimmune", "interferon gamma-1b")
-    ,`Generic Name` = replace(`Generic Name`, `Trade Name` == "bendeka", "bendamustine for 50ml admixture")
-    ,`Generic Name` = replace(`Generic Name`, `Trade Name` == "doxil", "doxorubicin liposome")
-    ,`Generic Name` = replace(`Generic Name`, `Trade Name` == "genotropin", "somatropin")
-    ,`Generic Name` = replace(`Generic Name`, `Trade Name` == "jakafi", "ruxolitinib phosphate")
-    ,`Generic Name` = replace(`Generic Name`, `Trade Name` == "gleevec", "imatinib mesylate")
-    ,`Generic Name` = replace(`Generic Name`, `Trade Name` == "humatrope", "somatropin")
-    ,`Generic Name` = replace(`Generic Name`, `Trade Name` == "norditropin", "somatropin")
-    ,`Generic Name` = replace(`Generic Name`, `Trade Name` == "novantrone", "mitoxantrone")
-    ,`Generic Name` = replace(`Generic Name`, `Trade Name` == "photofrin", "porfimer sodium")
-    ,`Generic Name` = replace(`Generic Name`, `Trade Name` == "photrexa viscous", "riboflavin ophthalmic solution ultraviolet-a (uva) irradiation")
-    ,`Generic Name` = replace(`Generic Name`, `Trade Name` == "salagen", "pilocarpine")
-    ,`Generic Name` = replace(`Generic Name`, `Trade Name` == "nutropin", "somatropin")
-    ,`Generic Name` = replace(`Generic Name`, `Trade Name` == "somatuline depot", "lanreotide acetate")
-    ,`Generic Name` = replace(`Generic Name`, `Trade Name` == "thyrogen", "thyrotropin alfa")
+    ,generic_name = replace(generic_name, trade_name == 'actimmune', 'interferon gamma-1b')
+    ,generic_name = replace(generic_name, trade_name == 'bendeka', 'bendamustine for 50ml admixture')
+    ,generic_name = replace(generic_name, trade_name == 'doxil', 'doxorubicin liposome')
+    ,generic_name = replace(generic_name, trade_name == 'genotropin', 'somatropin')
+    ,generic_name = replace(generic_name, trade_name == 'jakafi', 'ruxolitinib phosphate')
+    ,generic_name = replace(generic_name, trade_name == 'gleevec', 'imatinib mesylate')
+    ,generic_name = replace(generic_name, trade_name == 'humatrope', 'somatropin')
+    ,generic_name = replace(generic_name, trade_name == 'norditropin', 'somatropin')
+    ,generic_name = replace(generic_name, trade_name == 'novantrone', 'mitoxantrone')
+    ,generic_name = replace(generic_name, trade_name == 'photofrin', 'porfimer sodium')
+    ,generic_name = replace(generic_name, trade_name == 'photrexa viscous', 'riboflavin ophthalmic solution ultraviolet-a (uva) irradiation')
+    ,generic_name = replace(generic_name, trade_name == 'salagen', 'pilocarpine')
+    ,generic_name = replace(generic_name, trade_name == 'nutropin', 'somatropin')
+    ,generic_name = replace(generic_name, trade_name == 'somatuline depot', 'lanreotide acetate')
+    ,generic_name = replace(generic_name, trade_name == 'thyrogen', 'thyrotropin alfa')
    
     
     ,ind_id = 1:nrow(orphan_inds) 
@@ -53,16 +62,16 @@ orphan_inds <-
 ## There should be none if the manual changes worked
 n_distinct_generic_name <-
   orphan_inds %>%
-  filter(!is.na(`Trade Name`)) %>%
-  group_by(`Trade Name`) %>%
-  distinct(`Generic Name`) %>%
+  dplyr::filter(!is.na(trade_name)) %>%
+  group_by(trade_name) %>%
+  distinct(generic_name) %>%
   count()
 
 generic_multiples <- 
   orphan_inds %>% 
-  left_join(n_distinct_generic_name, by = "Trade Name") %>% 
-  select(`Trade Name`, everything()) %>% 
-  filter(n > 1)
+  left_join(n_distinct_generic_name, by = 'trade_name') %>% 
+  select(ind_id, trade_name, everything()) %>% 
+  dplyr::filter(n > 1)
 
 
 ## Assert no generic multiples
@@ -71,23 +80,23 @@ assert <- function(x) stopifnot(x)
 assert(nrow(generic_multiples) == 0)
 
 ## Print whether we were successful
-if(nrow(generic_multiples) == 0) print("Hooray, no drug brand names with multiple generic names!")
+if(nrow(generic_multiples) == 0) print('Hooray, no drug brand names with multiple generic names!')
 
 
 ## Count distinct orphan drugs
 ## The IMS Report indicates that 449 drugs were approved as of February 2017.
 orphan_inds %>% 
-  filter(mdy(`Marketing Approval Date`) < '2017-03-01') %>% 
-  select(`Trade Name`) %>% 
+  dplyr::filter(mdy(marketing_approval_date) < '2017-03-01') %>% 
+  select(trade_name) %>% 
   distinct() %>% 
-  filter(!is.na(`Trade Name`)) %>% 
+  dplyr::filter(!is.na(trade_name)) %>% 
   count()
 
 orphan_inds %>% 
-  filter(mdy(`Marketing Approval Date`) < '2017-03-01') %>% 
-  select(`Generic Name`) %>% 
+  dplyr::filter(mdy(marketing_approval_date) < '2017-03-01') %>% 
+  select(generic_name) %>% 
   distinct() %>%
-  filter(!is.na(`Generic Name`)) %>% 
+  dplyr::filter(!is.na(generic_name)) %>% 
   count()
 
 
