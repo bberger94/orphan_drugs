c018970d97113c6944b10950d8b053b63bec0edc
Ben Berger <benjaminaberger94@gmail.com>
2018-01-21 19:14:30 -0500

    Added script to export csv of orphan indications

---
+++ src/0-preprocess/export_csv.R
@@ -0,0 +1,29 @@
+
+# Export orphan indications to 
+
+## Load cleaned data from file if not in memory
+load('data/temp/2-clean_indications.RData')
+
+## Recode orphan as binary and rename variables
+inds_to_export <- 
+  orphan_inds %>% 
+    mutate(
+      orphan_indication = as.numeric(orphan_drug_status == 'Designated/Approved')
+    ) %>% 
+    select(
+      drug_id,
+      ind_id,
+      brand_name,
+      generic_name,
+      indication = approved_indication,
+      approval_date = marketing_approval_date,
+      orphan_indication,
+      orphan_designation_date = designation_date,
+      orphan_designation = designation,
+      orphan_exclusivity_end_date = exclusivity_end_date
+    ) 
+  
+## Write to csv
+write_csv(inds_to_export, 'data/orphan_indications/cleaned_orphan_indications_01-21-18.csv')
+
+
